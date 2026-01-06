ff8404267f1a2b80b767016971ae0521b2aab83a
Misono Toma <s24c1115dv@s.chibakoudai.jp>
2026-01-06 17:38:53 +0900

    add a command

--- era_converter/era_publisher.py
+++ era_converter/era_publisher.py
@@ -3,20 +3,21 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32
+from person_msgs.msg import Person
 
 class EraPublisher(Node):
     def __init__(self):
         super().__init__('era_publisher')
-        self.publisher_ = self.create_publisher(Int32, 'year', 10)
+        self.publisher_ = self.create_publisher(Person, 'year', 10)
         self.year = 1926
         self.timer = self.create_timer(1.0, self.timer_callback)
 
     def timer_callback(self):
-        msg = Int32()
-        msg.data = self.year
+        msg = Person()
+        msg.year = self.year
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publish: {self.year}')
+
         self.year += 1
         if self.year > 2025:
             self.year = 1926
--- era_converter/era_subscriber.py
+++ era_converter/era_subscriber.py
@@ -3,34 +3,33 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32
 
+from person_msgs.msg import Person
 
 class EraSubscriber(Node):
     def __init__(self):
         super().__init__('era_subscriber')
         self.subscription = self.create_subscription(
-            Int32,
+            Person,
             'year',
             self.listener_callback,
             10
         )
 
     def listener_callback(self, msg):
-        year = msg.data
+        year = msg.year
         era = ""
 
         if 1926 <= year <= 1988:
             era = f"昭和{year-1925}年"
         elif 1989 <= year <= 2018:
-            era = f"平成 {year-1988}年"
+            era = f"平成{year-1988}年"
         elif 2019 <= year <= 2025:
             era = f"令和{year-2018}年"
         else:
             era = "対象外"
 
-        self.get_logger().info(f"Received: {year}  →  {era}")
-
+        self.get_logger().info(f"Received: {year} → {era}")
 
 def main(args=None):
     rclpy.init(args=args)
@@ -38,3 +37,6 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
