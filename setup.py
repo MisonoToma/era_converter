7d8939ed06c2c3a7c51f7107692c9fb114c99026
Misono Toma <s24c1115dv@s.chibakoudai.jp>
2026-01-07 17:55:38 +0900

    change

--- setup.py
+++ setup.py
@@ -9,20 +9,17 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        # ament 用
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        # package.xml
         ('share/' + package_name, ['package.xml']),
-        # ★★★ これが重要 ★★★
         (os.path.join('share', package_name, 'launch'),
-            glob('era_converter/launch/*.launch.py')),
+            glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='MisonoToma',
     maintainer_email='s24c1115dv@s.chibakoudai.jp',
-    description='西暦を和暦（昭和・平成・令和）に変換するROS2ノード',
+    description='西暦変換',
     license='BSD-3-Clause',
     entry_points={
         'console_scripts': [
