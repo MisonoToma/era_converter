d8986a2a2963b4bd1eda4d973fc35dcd93ba7ed8
Misono Toma <s24c1115dv@s.chibakoudai.jp>
2026-01-07 19:27:15 +0900

    change

--- test/test.bash
+++ test/test.bash
@@ -7,8 +7,9 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source $dir/.bashrc
-timeout 10 ros2 launch era_converter era.launch.py > /tmp/era_converter.log
+source /opt/ros/humble/setup.bash 2>/dev/null || true
+[ -f install/setup.bash ] && source install/setup.bash
+timeout 10 ros2 launch era_converter era.launch.py > /tmp/era_converter.log 2>&1
 
-cat /tmp/era_converter.log |
-grep -E '昭和|平成|令和'
+grep -E '昭和|平成|令和' /tmp/era_converter.log || true
+
